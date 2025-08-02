5a918a889c194496b48fb0395f82db6add813412
reece <reece.tec@gmail.com>
2025-08-02 12:12:47 -0400

    init structure

---
+++ dags/hello_world.py
@@ -0,0 +1,15 @@
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="hello_world",
+    start_date=pendulum.datetime(2025, 8, 2, tz="UTC"),
+    catchup=False,
+    schedule=None,
+    tags=["example"],
+) as dag:
+    hello_task = BashOperator(
+        task_id="hello_task",
+        bash_command='echo "Hello from Airflow!"',
+    )
